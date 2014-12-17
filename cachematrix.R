eacb6cee39a3be18eae4ebc2f4ef640e84d3849b
leonvandenoord <leon@vandenoord.org>
2014-12-17 23:10:11 +0100

    Finished assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These methods together provide a mechanism to cache (and reuse)
+## the results of expensive computations.
 
-## Write a short comment describing this function
+## This method creates the object which stores the source matrix and
+## if/when calculated the inverse. When the matrix is changed/set the
+## calculated inverse is whiped.
 
 makeCacheMatrix <- function(x = matrix()) {
-testcommit
+  inverse <- NULL
+  set <- function(m) {
+    x <<- m
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(i) inverse <<- i
+  getInverse <- function() inverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve method first checks whether or not a cached result
+## is available. If availbe this is returned to the caller, alternatively
+## the computation (solve) is performed on the matrix and the result 
+## is stored in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  message("returning newly calculated inverse")
+  inverse
+}
+
+## Testing method to test our implementation.
+
+testCache <- function() {
+  message("testing below should be a calculation");
+  cache <- makeCacheMatrix(matrix(c(4,2,3,4),nrow=2, ncol=2))
+  cacheSolve(cache)
+  message("Cache hit should be below")
+  cacheSolve(cache)
+  cache$set(matrix(c(-2),nrow=1, ncol=1))
+  message("New matrix set, fresh value should be returned.")
+  cacheSolve(cache);
 }
